321c94678ac8d4b416922fae2fb80388d6d0beb9
Boo11 <i.butin0@gmail.com>
2026-02-05 09:28:52 +0000

    Fix edge case in authentication

---
+++ file_runner.py
@@ -0,0 +1,10 @@
+
+def r_plot_size(self) -> float:
+        """Track radius size for plot data (`r_size` with padding)"""
+        return max(self.r_plot_lim) - min(self.r_plot_lim)
+
+
+def name(self) -> str:
+        """Track name"""
+        return self._name
+
