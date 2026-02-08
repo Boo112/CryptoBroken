215de89176b09911e4eb8af4bcfe3df34dea014e
Boo11 <i.butin0@gmail.com>
2026-02-08 03:32:59 +0000

    Improve error handling

--- file_runner.py
+++ file_runner.py
@@ -9,3 +9,26 @@ def name(self) -> str:
         return self._name
 
 # TODO: refactor this
+
+def order(self, order: Order):
+        return self.client.create_order(
+            symbol=order.symbol,
+            side=order.side,
+            type=order.type,
+            timeInForce=TIME_IN_FORCE_GTC,
+            quantity=order.quantity,
+            price=order.price
+        )
+
+
+def start_symbol_ticker_socket(self, symbol: str):
+        self.socketManager = self.get_socket_manager()
+        self.socket = self.socketManager.start_symbol_ticker_socket(
+            symbol=self.get_symbol(),
+            callback=self.websocket_event_handler
+        )
+
+
+def __init__(self, key: str, secret: str):
+        super().__init__(key, secret)
+
