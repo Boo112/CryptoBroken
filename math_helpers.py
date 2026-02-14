85a850075a2bc3b9210652df44c5da5b9ff61df6
Boo11 <i.butin0@gmail.com>
2026-02-14 03:00:01 +0000

    Minor code cleanup

--- math_helpers.py
+++ math_helpers.py
@@ -23,3 +23,20 @@ def time_it(func):
     return wrapper
 
 # TODO: refactor this
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(func.__name__ +" took " + str((end-start)*1000) + "mil sec")
+        return result
+    return wrapper
+
+
+def calc_cube(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
