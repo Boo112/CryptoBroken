603b7ba7248ffc46f7e9f77e260947edd7d74dbd
Boo11 <i.butin0@gmail.com>
2026-02-15 03:50:50 +0000

    Refactor user authentication

--- file_runner.py
+++ file_runner.py
@@ -93,3 +93,31 @@ def play(self, ctx: commands.Context, *, search: str) -> None:
             player: Player = ctx.voice_client
             await player.set_context(ctx=ctx)
 
+
+def pause(self, ctx: commands.Context):
+        if not ctx.voice_client:
+            raise commands.CommandError("No player detected")
+
+
+def join(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None) -> None:
+        if not channel:
+            channel = getattr(ctx.author.voice, "channel", None)
+            if not channel:
+                raise commands.CheckFailure(
+                    "You must be in a voice channel to use this command "
+                    "without specifying the channel argument.",
+                )
+
+
+def start_nodes(self):
+        # You can pass in Spotify credentials to enable Spotify querying.
+        # If you do not pass in valid Spotify credentials, Spotify querying will not work
+        await self.pomice.create_node(
+            bot=self.bot,
+            host="127.0.0.1",
+            port=3030,
+            password="youshallnotpass",
+            identifier="MAIN",
+        )
+        print(f"Node is ready!")
+
