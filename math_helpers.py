d149d57dac483e54ebd32346bdeac8d5f49ab050
Boo11 <i.butin0@gmail.com>
2026-02-20 03:00:02 +0000

    Fix edge case in authentication

--- math_helpers.py
+++ math_helpers.py
@@ -42,3 +42,9 @@ def calc_cube(numbers):
 
 # TODO: refactor this
 # TODO: refactor this
+
+def historical_symbol_ticker_candle(self, start: datetime, end=None, interval=Client.KLINE_INTERVAL_1MINUTE):
+        # Convert default seconds interval to string like "1m"
+        if isinstance(interval, int):
+            interval = str(floor(interval/60)) + 'm'
+
