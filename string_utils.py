ab395149962078cee7dfb1daa680f1b33d5126c5
Boo11 <i.butin0@gmail.com>
2026-02-21 03:00:01 +0000

    Update dependencies

--- string_utils.py
+++ string_utils.py
@@ -22,3 +22,18 @@ def join(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None) -
                 )
 
 # TODO: refactor this
+
+def cancel_order(self, orderId):
+        return self.client.cancel_order(
+            symbol=self.get_symbol(),
+            orderId=orderId
+        )
+
+
+def start_symbol_ticker_socket(self, symbol: str):
+        self.socketManager = self.get_socket_manager()
+        self.socket = self.socketManager.start_symbol_ticker_socket(
+            symbol=self.get_symbol(),
+            callback=self.websocket_event_handler
+        )
+
