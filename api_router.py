6150712ce859bc58ddd6428892d58b1a2e068738
Boo11 <i.butin0@gmail.com>
2026-02-22 03:00:01 +0000

    Refactor user authentication

--- api_router.py
+++ api_router.py
@@ -3,3 +3,29 @@ def handle_article(self, art):
             self.querier.add_article(art)
 
 # TODO: refactor this
+
+def do_next(self) -> None:
+        # Clear the votes for a new song
+        self.pause_votes.clear()
+        self.resume_votes.clear()
+        self.skip_votes.clear()
+        self.shuffle_votes.clear()
+        self.stop_votes.clear()
+
+
+def play(self, ctx: commands.Context, *, search: str) -> None:
+        # Checks if the player is in the channel before we play anything
+        if not (player := ctx.voice_client):
+            await ctx.author.voice.channel.connect(cls=Player)
+            player: Player = ctx.voice_client
+            await player.set_context(ctx=ctx)
+
+
+def join(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None) -> None:
+        if not channel:
+            channel = getattr(ctx.author.voice, "channel", None)
+            if not channel:
+                return await ctx.send(
+                    "You must be in a voice channel in order to use this command!",
+                )
+
