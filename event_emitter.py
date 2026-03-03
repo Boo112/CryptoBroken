f35bb1911dc476ce953cf5495a5c8371e529e296
Boo11 <i.butin0@gmail.com>
2026-03-03 03:35:37 +0000

    Add comments and documentation

---
+++ event_emitter.py
@@ -0,0 +1,17 @@
+
+def handle_article(self, art):
+            self.querier.add_article(art)
+
+
+def _add_attribute_type(self, key, label, default_value=None):
+        """
+        Adds a new type of attribute to the list of attributes
+        understood by this query. Meant to be used by the constructors
+        in derived classes.
+        """
+        if len(self.attrs) == 0:
+            self.attrs[key] = [default_value, label, 0]
+            return
+        idx = max([item[2] for item in self.attrs.values()]) + 1
+        self.attrs[key] = [default_value, label, idx]
+
