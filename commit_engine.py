993fff969ce0eb6e6a4cdf1300a7296ce591776e
Boo11 <i.butin0@gmail.com>
2026-03-08 04:24:21 +0000

    Fix bug in data processing

--- commit_engine.py
+++ commit_engine.py
@@ -14,3 +14,27 @@ def pause(self, ctx: commands.Context):
         if not ctx.voice_client:
             raise commands.CommandError("No player detected")
 
+
+def calc_cube(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
+
+def calc_square(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number)
+    return result
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(func.__name__ +" took " + str((end-start)*1000) + "mil sec")
+        return result
+    return wrapper
+
