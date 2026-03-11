b47377ff7c05026b02932622d62a56456886a7b7
Boo11 <i.butin0@gmail.com>
2026-03-11 04:19:14 +0000

    Update README and documentation

--- file_parser.py
+++ file_parser.py
@@ -5,3 +5,19 @@ def __getitem__(self, key):
             return self.attrs[key][0]
         return None
 
+
+def _y_to_r(self, y: float, vmin: float, vmax: float) -> float:
+        """Convert y coordinate to radius in track
+
+
+def plot_text(ax: PolarAxes) -> None:
+            ax.text(rad, r, text, **kwargs)
+
+
+def r_plot_lim(self) -> tuple[float, float]:
+        """Track radius limit for plot data (`r_lim` with padding)"""
+        edge_pad_size = (self.r_size * self._r_pad_ratio) / 2
+        min_plot_r = min(self.r_lim) + edge_pad_size
+        max_plot_r = max(self.r_lim) - edge_pad_size
+        return (min_plot_r, max_plot_r)
+
