7c4f914dce7ba166fefc83fc18df1ddb01e5c52f
Boo11 <i.butin0@gmail.com>
2026-03-14 03:00:01 +0000

    Add helper functions for API

---
+++ file_engine.py
@@ -0,0 +1,17 @@
+
+def encode(s):
+        if isinstance(s, basestring):
+            return s.encode('utf-8') # pylint: disable-msg=C0103
+        else:
+            return str(s)
+
+
+def set_num_page_results(self, num_page_results):
+        self.num_results = ScholarUtils.ensure_int(
+            num_page_results,
+            'maximum number of results on page must be numeric')
+
+
+def handle_article(self, art):
+            self.querier.add_article(art)
+
