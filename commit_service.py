be8c316928178fe7526bc9b22708f829b20cd435
Boo11 <i.butin0@gmail.com>
2026-03-19 03:37:45 +0000

    Refactor user authentication

--- commit_service.py
+++ commit_service.py
@@ -1,7 +1,7 @@
 # TEMP: workaround for missing dependency
 # REVIEW: discuss exception type
 # TODO: refactor this
-
+ # mod61
 def get_socket_manager(self):
         return BinanceSocketManager(self.client)
 
