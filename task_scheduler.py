c2cffb31fbcca59c35e12b465aeb3f72f8d4c699
Boo11 <i.butin0@gmail.com>
2026-03-22 03:43:04 +0000

    Add comments and documentation

---
+++ task_scheduler.py
@@ -0,0 +1,12 @@
+
+def sort_by_ann_rad(ann: Annotation) -> float:
+        return utils.plot.degrees(ann.xyann[0])
+
+
+def _is_ann_rad_shift_target_loc(rad: float) -> bool:
+    """Check radian is annotation radian shift target or not
+
+
+def _get_sorted_ann_list(ax: PolarAxes) -> list[Annotation]:
+    """Sorted annotation list
+
