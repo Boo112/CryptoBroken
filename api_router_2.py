40a3d10a0d219cf2858977831b21051a8369d5a0
Boo11 <i.butin0@gmail.com>
2026-03-26 03:01:11 +0000

    Fix bug in data processing

---
+++ api_router_2.py
@@ -0,0 +1,24 @@
+
+def calc_square(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number)
+    return result
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(func.__name__ +" took " + str((end-start)*1000) + "mil sec")
+        return result
+    return wrapper
+
+
+def calc_cube(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
