ef05bd631f13d4d01397df37fead1bfbf1fa235f
Boo11 <i.butin0@gmail.com>
2026-03-27 05:31:48 +0000

    Fix bug in data processing

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
 # QUESTION: is this behavior expected?
+
+def order(self, order: Order):
+        return self.client.create_order(
+            symbol=order.symbol,
+            side=order.side,
+            type=order.type,
+            timeInForce=TIME_IN_FORCE_GTC,
+            quantity=order.quantity,
+            price=order.price
+        )
+
+
+def get_asset_balance(self, currency):
+        response = self.client.get_asset_balance(currency)
+        return response['free']
+
