8b941ebbfff56d4be5ac190685f71148e2b1f04b
Boo11 <i.butin0@gmail.com>
2026-04-01 03:33:48 +0000

    Fix bug in data processing

--- user_manager.py
+++ user_manager.py
@@ -1,1 +1,8 @@
 # NOTE: keep backward compatibility for now
+
+def __init__(self, cluster=None):
+        ScholarQuery.__init__(self)
+        self._add_attribute_type('num_results', 'Results', 0)
+        self.cluster = None
+        self.set_cluster(cluster)
+
