8320ec08b075dfa65a2684d88afd7f0f293ee163
Boo11 <i.butin0@gmail.com>
2026-04-03 04:17:32 +0000

    Refactor helper functions

--- api_router.py
+++ api_router.py
@@ -70,3 +70,25 @@ def resume(self, ctx: commands.Context):
 def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+
+def get_ann_window_extent(ann: Annotation) -> Bbox:
+        return Text.get_window_extent(ann).expanded(*config.ann_adjust.expand)
+
+
+def sort_by_ann_rad(ann: Annotation) -> float:
+        return utils.plot.degrees(ann.xyann[0])
+
+
+def adjust_annotation(ax: PolarAxes) -> None:
+    """Adjust annotation text position"""
+    # Get sorted annotation list for position adjustment
+    ann_list = _get_sorted_ann_list(ax)
+    if len(ann_list) == 0 or config.ann_adjust.max_iter <= 0:
+        return
+    if len(ann_list) > config.ann_adjust.limit:
+        warnings.warn(
+            f"Too many annotations(={len(ann_list)}). Annotation position adjustment is not done.",  # noqa: E501
+            stacklevel=2,
+        )
+        return
+
