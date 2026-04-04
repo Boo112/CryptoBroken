0d3b80571897b18ae766d609ff66b71a140ecec5
Boo11 <i.butin0@gmail.com>
2026-04-04 04:50:23 +0000

    Update dependencies

--- utils.py
+++ utils.py
@@ -16,3 +16,21 @@ def get_asset_balance(self, currency):
         return response['free']
 # TODO: support retry mechanism
 
+
+def adjust_annotation(ax: PolarAxes) -> None:
+    """Adjust annotation text position"""
+    # Get sorted annotation list for position adjustment
+    ann_list = _get_sorted_ann_list(ax)
+    if len(ann_list) == 0 or config.ann_adjust.max_iter <= 0:
+        return
+    if len(ann_list) > config.ann_adjust.limit:
+        warnings.warn(
+            f"Too many annotations(={len(ann_list)}). Annotation position adjustment is not done.",  # noqa: E501
+            stacklevel=2,
+        )
+        return
+
+
+def sort_by_ann_rad(ann: Annotation) -> float:
+        return utils.plot.degrees(ann.xyann[0])
+
