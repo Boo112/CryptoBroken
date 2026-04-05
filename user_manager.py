775b68351755adb85d742695d26bf75d11f093a2
Boo11 <i.butin0@gmail.com>
2026-04-05 03:36:11 +0000

    Add helper functions for API

--- user_manager.py
+++ user_manager.py
@@ -6,3 +6,25 @@ def __init__(self, cluster=None):
         self.cluster = None
         self.set_cluster(cluster)
 
+
+def join(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None) -> None:
+        if not channel:
+            channel = getattr(ctx.author.voice, "channel", None)
+            if not channel:
+                return await ctx.send(
+                    "You must be in a voice channel in order to use this command!",
+                )
+
+
+def is_privileged(self, ctx: commands.Context):
+        """Check whether the user is an Admin or DJ."""
+        player: Player = ctx.voice_client
+
+
+def teardown(self):
+        """Clear internal states, remove player controller and disconnect."""
+        with suppress((discord.HTTPException), (KeyError)):
+            await self.destroy()
+            if self.controller:
+                await self.controller.delete()
+
