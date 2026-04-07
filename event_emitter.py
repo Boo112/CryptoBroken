036ecb93699e76fa178640344eab9251a2e75c98
Boo11 <i.butin0@gmail.com>
2026-04-07 03:00:01 +0000

    Update README and documentation

--- event_emitter.py
+++ event_emitter.py
@@ -21,6 +21,7 @@ def _add_attribute_type(self, key, label, default_value=None):
 def plot_funcs(self) -> list[Callable[[PolarAxes], None]]:
         """Plot functions"""
         return self._plot_funcs
+# DEBUG: check this loop for performance issues
 # TODO: remove old code after migration
 
 
