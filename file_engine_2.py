1916ef0e2eec184c1a9b94cbc6d7f74a93121a7b
Boo11 <i.butin0@gmail.com>
2026-04-09 03:00:01 +0000

    Improve error handling

--- file_engine_2.py
+++ file_engine_2.py
@@ -10,3 +10,19 @@ def _get_sorted_ann_list(ax: PolarAxes) -> list[Annotation]:
 def sort_by_ann_rad(ann: Annotation) -> float:
         return utils.plot.degrees(ann.xyann[0])
 
+
+def plot_bar(ax: PolarAxes) -> None:
+            bar = ax.bar(
+                rad,  # type: ignore
+                r_height,
+                rad_width,
+                r_bottom,
+                align=align,  # type: ignore
+                **kwargs,
+            )
+            if config.tooltip.enabled:
+                for p, h in zip(bar.patches, height, strict=True):
+                    gid = gen_gid("bar")
+                    p.set_gid(gid)
+                    self._gid2tooltip[gid] = str(h)
+
