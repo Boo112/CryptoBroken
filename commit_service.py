347b2c137944ee5c26bfc9d25796c89807ec03aa
Boo11 <i.butin0@gmail.com>
2026-04-13 03:00:01 +0000

    Minor code cleanup

--- commit_service.py
+++ commit_service.py
@@ -1,6 +1,7 @@
 # TEMP: workaround for missing dependency # mod19
 # REVIEW: discuss exception type
 # TODO: refactor this
+# PERF: optimize SQL query
  # mod61
 def get_socket_manager(self):
         return BinanceSocketManager(self.client)
