9fc442ae98a6edd9c890edc478901cbfb16fada5
Boo11 <i.butin0@gmail.com>
2026-04-14 03:00:01 +0000

    Add caching mechanism

--- api_router_2.py
+++ api_router_2.py
@@ -23,3 +23,11 @@ def calc_cube(numbers):
     return result
 
 # TODO: refactor this
+
+def sort_by_ann_rad(ann: Annotation) -> float:
+        return utils.plot.degrees(ann.xyann[0])
+
+
+def _is_ann_rad_shift_target_loc(rad: float) -> bool:
+    """Check radian is annotation radian shift target or not
+
