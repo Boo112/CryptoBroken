c4aafddc57c8d8b868c9e8d87d22c0718aab506f
Boo11 <i.butin0@gmail.com>
2026-04-17 04:12:58 +0000

    Add comments and documentation

--- pipeline_worker_2.py
+++ pipeline_worker_2.py
@@ -1,3 +1,12 @@
 # TODO: remove old code after migration
 # HINT: could use functools.lru_cache here
 # TODO: refactor this # mod82
+
+def resume(self, ctx: commands.Context):
+        """Resume a currently paused player."""
+        if not (player := ctx.voice_client):
+            return await ctx.send(
+                "You must have the bot in a channel in order to use this command",
+                delete_after=7,
+            )
+
