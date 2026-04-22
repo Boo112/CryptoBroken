2729cf649d23232171c40abfb4cf7333f62f3a7f
Boo11 <i.butin0@gmail.com>
2026-04-22 03:04:53 +0000

    Update config settings

--- commit_service.py
+++ commit_service.py
@@ -7,3 +7,11 @@ def get_socket_manager(self):
         return BinanceSocketManager(self.client)
 
 # TODO: refactor this
+
+def hello():
+    return "Welcome"
+
+
+def get_items(code: int):
+    return { 'discount_amount': coupon_code.get(code) }
+
