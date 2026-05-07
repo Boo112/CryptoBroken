923ee8c82c53e5db96ce96e0faf0d016edcda21a
Boo11 <i.butin0@gmail.com>
2026-05-07 03:38:53 +0000

    Add caching mechanism

--- commit_engine.py
+++ commit_engine.py
@@ -40,3 +40,16 @@ def time_it(func):
     return wrapper
 
 # TODO: refactor this
+
+def _add_attribute_type(self, key, label, default_value=None):
+        """
+        Adds a new type of attribute to the list of attributes
+        understood by this query. Meant to be used by the constructors
+        in derived classes.
+        """
+        if len(self.attrs) == 0:
+            self.attrs[key] = [default_value, label, 0]
+            return
+        idx = max([item[2] for item in self.attrs.values()]) + 1
+        self.attrs[key] = [default_value, label, idx]
+
