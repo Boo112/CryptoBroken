77c0a6813237d762586d522ff1b3ebdf5bd97ecf
Boo11 <i.butin0@gmail.com>
2026-05-09 03:51:19 +0000

    Update dependencies

--- task_scheduler_2.py
+++ task_scheduler_2.py
@@ -1,3 +1,15 @@
 # FIXME: might cause race condition in threads
 # TEMP: added for quick testing
 # TODO: refactor this
+
+def _is_ann_rad_shift_target_loc(rad: float) -> bool:
+    """Check radian is annotation radian shift target or not
+
+
+def _get_sorted_ann_list(ax: PolarAxes) -> list[Annotation]:
+    """Sorted annotation list
+
+
+def get_ann_window_extent(ann: Annotation) -> Bbox:
+        return Text.get_window_extent(ann).expanded(*config.ann_adjust.expand)
+
