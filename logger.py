f438568cdda991e4c52427db8a5e495583ecedc6
Boo11 <i.butin0@gmail.com>
2026-05-12 03:00:01 +0000

    gogo

---
+++ logger.py
@@ -0,0 +1,11 @@
+
+def symbol_ticker(self):
+        response = self.client.get_symbol_ticker(symbol=self.get_symbol())
+        print(response)
+        return Price(pair=self.get_symbol(), currency=self.currency.lower(), asset=self.asset.lower(), exchange=self.name.lower(),
+                     current=response['price'], openAt=utils.format_date(datetime.now()))
+
+
+def get_socket_manager(self):
+        return BinanceSocketManager(self.client)
+
