0c8f4f2ede083356dc60ac5a81a0b685b3a0dfd1
Boo11 <i.butin0@gmail.com>
2026-05-15 05:41:49 +0000

    Fix edge case in authentication

--- task_scheduler_2.py
+++ task_scheduler_2.py
@@ -13,3 +13,29 @@ def _get_sorted_ann_list(ax: PolarAxes) -> list[Annotation]:
 def get_ann_window_extent(ann: Annotation) -> Bbox:
         return Text.get_window_extent(ann).expanded(*config.ann_adjust.expand)
 
+
+def check_order(self, orderId):
+        return self.client.get_order(
+            symbol=self.get_symbol(),
+            orderId=orderId
+        )
+
+
+def start_symbol_ticker_socket(self, symbol: str):
+        self.socketManager = self.get_socket_manager()
+        self.socket = self.socketManager.start_symbol_ticker_socket(
+            symbol=self.get_symbol(),
+            callback=self.websocket_event_handler
+        )
+
+
+def test_order(self, order: Order):
+        return self.client.create_test_order(
+            symbol=order.symbol,
+            side=order.side,
+            type=order.type,
+            timeInForce=TIME_IN_FORCE_GTC,
+            quantity=order.quantity,
+            price=order.price
+        )
+
