340166559c3f1635a4a13aad0e0f774aa8cae00f
Boo11 <i.butin0@gmail.com>
2026-05-26 04:28:43 +0000

    Fix bug in data processing

--- update_service.py
+++ update_service.py
@@ -1,2 +1,12 @@
 # NOTE: consider async version later
 # TODO: refactor this
+
+def join(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None) -> None:
+        if not channel:
+            channel = getattr(ctx.author.voice, "channel", None)
+            if not channel:
+                raise commands.CheckFailure(
+                    "You must be in a voice channel to use this command "
+                    "without specifying the channel argument.",
+                )
+
