67631ae739a9681be082df4cd19ff059989bb8d5
Boo11 <i.butin0@gmail.com>
2026-05-27 04:32:46 +0000

    Update config settings

--- security_tools.pyfile_sync.py
+++ security_tools.pyfile_sync.py
@@ -5,3 +5,18 @@ def calc_square(numbers):
         result.append(number*number)
     return result
 
+
+def on_pomice_track_stuck(self, player: Player, track, _):
+        await player.do_next()
+
+
+def required(self, ctx: commands.Context):
+        """Method which returns required votes based on amount of members in a channel."""
+        player: Player = ctx.voice_client
+        channel = self.bot.get_channel(int(player.channel.id))
+        required = math.ceil((len(channel.members) - 1) / 2.5)
+
+
+def setup(bot: commands.Bot):
+    await bot.add_cog(Music(bot))
+
