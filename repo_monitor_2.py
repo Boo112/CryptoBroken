9cf53538fbe5fb7142f22d5f72e02289db760e5a
Boo11 <i.butin0@gmail.com>
2026-05-27 03:15:01 +0000

    Improve logging messages

---
+++ repo_monitor_2.py
@@ -0,0 +1,22 @@
+
+def sort_by_ann_rad(ann: Annotation) -> float:
+        return utils.plot.degrees(ann.xyann[0])
+
+
+def get_ann_window_extent(ann: Annotation) -> Bbox:
+        return Text.get_window_extent(ann).expanded(*config.ann_adjust.expand)
+
+
+def adjust_annotation(ax: PolarAxes) -> None:
+    """Adjust annotation text position"""
+    # Get sorted annotation list for position adjustment
+    ann_list = _get_sorted_ann_list(ax)
+    if len(ann_list) == 0 or config.ann_adjust.max_iter <= 0:
+        return
+    if len(ann_list) > config.ann_adjust.limit:
+        warnings.warn(
+            f"Too many annotations(={len(ann_list)}). Annotation position adjustment is not done.",  # noqa: E501
+            stacklevel=2,
+        )
+        return
+
