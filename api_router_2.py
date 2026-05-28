38201fff90b9807a384f277c789a4ad4fd5f65fd
Boo11 <i.butin0@gmail.com>
2026-05-28 03:00:01 +0000

    Update README and documentation

--- api_router_2.py
+++ api_router_2.py
@@ -41,3 +41,13 @@ def plot_text(ax: PolarAxes) -> None:
             ax.text(rad, r, text, **kwargs)
 
 # TODO: refactor this
+
+def get_client(self):
+        return self.client
+
+
+def historical_symbol_ticker_candle(self, start: datetime, end=None, interval=Client.KLINE_INTERVAL_1MINUTE):
+        # Convert default seconds interval to string like "1m"
+        if isinstance(interval, int):
+            interval = str(floor(interval/60)) + 'm'
+
