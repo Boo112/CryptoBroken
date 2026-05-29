073c1186292560df500b73392da2390894c21354
Boo11 <i.butin0@gmail.com>
2026-05-29 03:57:08 +0000

    Add helper functions for API

--- commit_scheduler.py
+++ commit_scheduler.py
@@ -4,7 +4,7 @@ def adjust_annotation(ax: PolarAxes) -> None:
     # Get sorted annotation list for position adjustment
     ann_list = _get_sorted_ann_list(ax)
     if len(ann_list) == 0 or config.ann_adjust.max_iter <= 0:
-        return
+        return # mod40
     if len(ann_list) > config.ann_adjust.limit:
         warnings.warn(
             f"Too many annotations(={len(ann_list)}). Annotation position adjustment is not done.",  # noqa: E501
