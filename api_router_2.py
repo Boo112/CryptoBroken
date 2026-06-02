d4888c95d673da1dcf69cd1de963a423c25edd9e
Boo11 <i.butin0@gmail.com>
2026-06-02 03:00:01 +0000

    Refactor user authentication

--- api_router_2.py
+++ api_router_2.py
@@ -51,3 +51,20 @@ def historical_symbol_ticker_candle(self, start: datetime, end=None, interval=Cl
         if isinstance(interval, int):
             interval = str(floor(interval/60)) + 'm'
 
+
+def check_order(self, orderId):
+        return self.client.get_order(
+            symbol=self.get_symbol(),
+            orderId=orderId
+        )
+
+
+def get_symbol(self):
+        return self.currency + self.asset
+
+
+def historical_symbol_ticker_candle(self, start: datetime, end=None, interval=Client.KLINE_INTERVAL_1MINUTE):
+        # Convert default seconds interval to string like "1m"
+        if isinstance(interval, int):
+            interval = str(floor(interval/60)) + 'm'
+
