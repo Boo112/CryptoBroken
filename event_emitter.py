4688d735bb85ad78265cc78656de74598cbed83b
Boo11 <i.butin0@gmail.com>
2026-06-03 04:16:52 +0000

    Add caching mechanism

--- event_emitter.py
+++ event_emitter.py
@@ -77,3 +77,10 @@ def calc_cube(numbers):
         result.append(number*number*number)
     return result
 
+
+def calc_cube(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
