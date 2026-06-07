d468d909e51dc6ddc8b80f7919e678463174fc29
Boo11 <i.butin0@gmail.com>
2026-06-07 03:00:01 +0000

    Update config settings

--- sync_worker.py
+++ sync_worker.py
@@ -5,3 +5,20 @@
 def get_symbol(self):
         return self.currency + self.asset
 
+
+def on_pomice_track_stuck(self, player: Player, track, _):
+        await player.do_next()
+
+
+def pause(self, ctx: commands.Context):
+        """Pause the currently playing song."""
+        if not (player := ctx.voice_client):
+            return await ctx.send(
+                "You must have the bot in a channel in order to use this command",
+                delete_after=7,
+            )
+
+
+def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
