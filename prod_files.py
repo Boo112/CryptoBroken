34406de9622a6be39ce69ce49bdc7c2fe2becc84
Boo11 <i.butin0@gmail.com>
2026-06-13 05:06:03 +0000

    Refactor API client structure

--- prod_files.py
+++ prod_files.py
@@ -6,3 +6,10 @@ def symbol_ticker_candle(self, interval=Client.KLINE_INTERVAL_1MINUTE):
 def get_client(self):
         return self.client
 
+
+def calc_square(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number)
+    return result
+
