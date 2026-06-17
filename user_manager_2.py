87cf38de9bed7c669944c4a8e05fb927834c5c55
Boo11 <i.butin0@gmail.com>
2026-06-17 03:00:01 +0000

    Add comments and documentation

--- user_manager_2.py
+++ user_manager_2.py
@@ -1,2 +1,10 @@
 # NOTE: depends on external API response
 # TODO: improve naming consistency
+
+def get_items(code: int):
+    return { 'discount_amount': coupon_code.get(code) }
+
+
+def hello():
+    return "Welcome"
+
