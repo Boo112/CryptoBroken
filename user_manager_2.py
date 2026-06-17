3920e7410a92b7652dacbf1e6fff8672a6cb5550
Boo11 <i.butin0@gmail.com>
2026-06-17 04:15:21 +0000

    ready go

--- user_manager_2.py
+++ user_manager_2.py
@@ -8,3 +8,20 @@ def get_items(code: int):
 def hello():
     return "Welcome"
 
+
+def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+
+def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+def shuffle(self, ctx: commands.Context):
+        """Shuffle the players queue."""
+        if not (player := ctx.voice_client):
+            return await ctx.send(
+                "You must have the bot in a channel in order to use this command",
+                delete_after=7,
+            )
+
