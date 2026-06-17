0722a1addf0d2534a6091012d7137c6d5093ff8a
Boo11 <i.butin0@gmail.com>
2026-06-17 03:31:19 +0000

    Add unit tests for models

--- api_router.py
+++ api_router.py
@@ -130,3 +130,20 @@ def plot_raster(ax: PolarAxes) -> None:
             ax.pcolormesh(rad_list, r_list, np.array(resize_img), **kwargs)
 
 # TODO: refactor this
+
+def calc_square(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number)
+    return result
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(func.__name__ +" took " + str((end-start)*1000) + "mil sec")
+        return result
+    return wrapper
+
