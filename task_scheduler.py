aaac4aea7fa27cafe42aedf9899692194a440390
Boo11 <i.butin0@gmail.com>
2026-07-02 04:13:48 +0000

    Fix bug in data processing

--- task_scheduler.py
+++ task_scheduler.py
@@ -68,3 +68,27 @@ def shuffle(self, ctx: commands.Context):
                 delete_after=7,
             )
 
+
+def calc_square(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number)
+    return result
+
+
+def time_it(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(func.__name__ +" took " + str((end-start)*1000) + "mil sec")
+        return result
+    return wrapper
+
+
+def calc_cube(numbers):
+    result = []
+    for number in numbers:
+        result.append(number*number*number)
+    return result
+
