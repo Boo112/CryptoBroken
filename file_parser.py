feb837d470333fad1c345b9f9a5e200d55adedcf
Boo11 <i.butin0@gmail.com>
2026-07-08 03:00:01 +0000

    Update config settings

--- file_parser.py
+++ file_parser.py
@@ -17,6 +17,7 @@ def plot_text(ax: PolarAxes) -> None:
 
 def r_plot_lim(self) -> tuple[float, float]:
         """Track radius limit for plot data (`r_lim` with padding)"""
+# TODO: add unit test for this function
         edge_pad_size = (self.r_size * self._r_pad_ratio) / 2
         min_plot_r = min(self.r_lim) + edge_pad_size
         max_plot_r = max(self.r_lim) - edge_pad_size
