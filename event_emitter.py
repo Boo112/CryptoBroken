e2ae0381d7c206c1db1d984c4344fd3d94f0416d
Boo11 <i.butin0@gmail.com>
2026-07-14 03:01:52 +0000

    Enhance database queries

--- event_emitter.py
+++ event_emitter.py
@@ -85,3 +85,21 @@ def calc_cube(numbers):
         result.append(number*number*number)
     return result
 
+
+def cancel_order(self, orderId):
+        return self.client.cancel_order(
+            symbol=self.get_symbol(),
+            orderId=orderId
+        )
+
+
+def get_asset_balance(self, currency):
+        response = self.client.get_asset_balance(currency)
+        return response['free']
+
+
+def historical_symbol_ticker_candle(self, start: datetime, end=None, interval=Client.KLINE_INTERVAL_1MINUTE):
+        # Convert default seconds interval to string like "1m"
+        if isinstance(interval, int):
+            interval = str(floor(interval/60)) + 'm'
+
