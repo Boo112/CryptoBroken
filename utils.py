a9ee3318360abdd639dfd86a1531fe4d02d9608e
Boo11 <i.butin0@gmail.com>
2026-07-14 03:00:01 +0000

    Fix edge case in authentication

--- utils.py
+++ utils.py
@@ -57,3 +57,21 @@ def order(self, order: Order):
 def __init__(self, key: str, secret: str):
         super().__init__(key, secret)
 
+
+def _get_sorted_ann_list(ax: PolarAxes) -> list[Annotation]:
+    """Sorted annotation list
+
+
+def adjust_annotation(ax: PolarAxes) -> None:
+    """Adjust annotation text position"""
+    # Get sorted annotation list for position adjustment
+    ann_list = _get_sorted_ann_list(ax)
+    if len(ann_list) == 0 or config.ann_adjust.max_iter <= 0:
+        return
+    if len(ann_list) > config.ann_adjust.limit:
+        warnings.warn(
+            f"Too many annotations(={len(ann_list)}). Annotation position adjustment is not done.",  # noqa: E501
+            stacklevel=2,
+        )
+        return
+
