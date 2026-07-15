6f336b33cfb2e6d5c67a38f32cc6ba5879eb6cc5
Boo11 <i.butin0@gmail.com>
2026-07-15 04:01:38 +0000

    gogo

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 # FIXME: string encoding issue possible here
 # TODO: refactor this
 # TODO: refactor this
+
+def start_symbol_ticker_socket(self, symbol: str):
+        self.socketManager = self.get_socket_manager()
+        self.socket = self.socketManager.start_symbol_ticker_socket(
+            symbol=self.get_symbol(),
+            callback=self.websocket_event_handler
+        )
+
